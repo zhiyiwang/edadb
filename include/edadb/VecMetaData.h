f0b51f9a88a19088393720b9cd180d3f3b2db830
zhiyi <zhiyiwang@aliyun.com>
2025-04-27 10:31:29 +0800

    VecMetaData: add new file

---
+++ include/edadb/VecMetaData.h
@@ -0,0 +1,20 @@
+/**
+ * @file VecMetaData.h
+ * @brief VecMetaData.h provides a way to get the meta data of a class with vector.
+ */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+namespace edadb {
+
+/**
+ * @brief VecMetaData provides a way to get the meta data of a class with vector
+ * @tparam T The class type.
+ */
+template<typename T>
+struct VecMetaData;
+
+} // namespace edadb
